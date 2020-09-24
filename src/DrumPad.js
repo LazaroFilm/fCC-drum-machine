6edc7ee8e35760fae786be186eadc344e45c8596
Victor Lazaro <lazarofilm@gmail.com>
2020-09-24 18:05:40 -0400

    first sound!

--- src/DrumPad.js
+++ src/DrumPad.js
@@ -1,67 +1,118 @@
+import { findAllByTestId } from "@testing-library/react";
 import React, { Component } from "react";
 import { Button, Container, Row, Col } from "react-bootstrap";
 
+const test = "Hello World";
+const pads = {
+  Q: {
+    sound: `Chord 1`,
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
+  },
+  W: {
+    sound: `Chord 2`,
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
+  },
+  E: {
+    sound: `Chord 3`,
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
+  },
+  A: {
+    sound: `Shaker`,
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
+  },
+  S: {
+    sound: `Open HH`,
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
+  },
+  D: {
+    sound: `Closed HH`,
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3",
+  },
+  Z: {
+    sound: `Punchy Kick`,
+    url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
+  },
+  X: {
+    sound: `Side Stick`,
+    url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
+  },
+  C: {
+    sound: `Snare`,
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
+  },
+};
+
 class DrumPad extends Component {
-  state = {
-    pads: {
-      Q: {
-        sound: `Chord 1`,
-        url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
-      },
-      W: {
-        sound: `Chord 2`,
-        url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
-      },
-      E: {
-        sound: `Chord 3`,
-        url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
-      },
-      A: {
-        sound: `Shaker`,
-        url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
-      },
-      S: {
-        sound: `Open HH`,
-        url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
-      },
-      D: {
-        sound: `Closed HH`,
-        url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3",
-      },
-      Z: {
-        sound: `Punchy Kick`,
-        url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
-      },
-      X: {
-        sound: `Side Stick`,
-        url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
-      },
-      C: {
-        sound: `Snare`,
-        url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
-      },
-    },
-  };
+  // state = {
+  //   pads: {
+  //     Q: {
+  //       sound: `Chord 1`,
+  //       url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
+  //     },
+  //     W: {
+  //       sound: `Chord 2`,
+  //       url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
+  //     },
+  //     E: {
+  //       sound: `Chord 3`,
+  //       url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
+  //     },
+  //     A: {
+  //       sound: `Shaker`,
+  //       url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
+  //     },
+  //     S: {
+  //       sound: `Open HH`,
+  //       url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
+  //     },
+  //     D: {
+  //       sound: `Closed HH`,
+  //       url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3",
+  //     },
+  //     Z: {
+  //       sound: `Punchy Kick`,
+  //       url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
+  //     },
+  //     X: {
+  //       sound: `Side Stick`,
+  //       url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
+  //     },
+  //     C: {
+  //       sound: `Snare`,
+  //       url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
+  //     },
+  //   },
+  // };
 
   makeButton(padName) {
     return (
       <Col className="p-1">
         <Button
           className="drum-pad"
-          id={this.state.pads[padName][1]}
+          id={padName}
           variant="primary btn-lg btn-block py-5"
           type="submit"
+          onClick={this.handleClick}
         >
           {padName}
-          <br />
-          {this.state.pads[padName].sound}
+          {/* <br />
+          {pads[padName].sound} */}
+          <audio
+            className="clip"
+            id="audioClip"
+            src="https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3"
+            type="audio/mpeg"
+          ></audio>
         </Button>
       </Col>
     );
   }
 
   handleClick() {
+    const audioClip = document.getElementById("audioClip");
     console.log(`clicked!`);
+    console.log(pads["Q"].sound);
+    audioClip.play();
   }
 
   render() {
