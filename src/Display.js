5fa843277165a3f7426ad03dbff30de70862afd2
Victor Lazaro <lazarofilm@gmail.com>
2020-09-24 16:56:17 -0400

    Added sounds urls and Nav display

--- src/Display.js
+++ src/Display.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import { Alert, Navbar } from "react-bootstrap";
 
 class Display extends Component {
   render() {
-    return <div id="display">Display goes here</div>;
+    return (
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand>Dr(u/ea)m Machine</Navbar.Brand>
+        <Alert variant="primary" id="display">
+          Display goes here
+        </Alert>
+      </Navbar>
+    );
   }
 }
 
--- src/DrumPad.js
+++ src/DrumPad.js
@@ -1,19 +1,45 @@
 import React, { Component } from "react";
-// import Button from "react-bootstrap/Button";
 import { Button, Container, Row, Col } from "react-bootstrap";
 
 class DrumPad extends Component {
   state = {
     pads: {
-      Q: `boom`,
-      W: `baf`,
-      E: `burp`,
-      A: `kaboom`,
-      S: `sploosh`,
-      D: `bark`,
-      Z: `moo`,
-      X: `bzzz`,
-      C: `katchoo`,
+      Q: {
+        sound: `Chord 1`,
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
+      },
+      W: {
+        sound: `Chord 2`,
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
+      },
+      E: {
+        sound: `Chord 3`,
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
+      },
+      A: {
+        sound: `Shaker`,
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
+      },
+      S: {
+        sound: `Open HH`,
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
+      },
+      D: {
+        sound: `Closed HH`,
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3",
+      },
+      Z: {
+        sound: `Punchy Kick`,
+        url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
+      },
+      X: {
+        sound: `Side Stick`,
+        url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
+      },
+      C: {
+        sound: `Snare`,
+        url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
+      },
     },
   };
 
@@ -22,14 +48,20 @@ class DrumPad extends Component {
       <Col className="p-1">
         <Button
           className="drum-pad"
-          id={this.state.pads[padName]}
+          id={this.state.pads[padName][1]}
           variant="primary btn-lg btn-block py-5"
           type="submit"
         >
           {padName}
+          <br />
+          {this.state.pads[padName].sound}
         </Button>
       </Col>
     );
+  }
+
+  handleClick() {
+    console.log(`clicked!`);
   }
 
   render() {
