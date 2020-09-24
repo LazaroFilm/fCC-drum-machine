e09a405383fc28390299ec0a15fe6f8161c27dd3
Victor Lazaro <lazarofilm@gmail.com>
2020-09-24 12:25:39 -0400

    Created Display & DrumPad

---
+++ src/Display.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Display extends Component {
+  render() {
+    return <div id="display">Display goes here</div>;
+  }
+}
+
+export default Display;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Display from "./Display";
+import DrumPad from "./DrumPad";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div id="drum-machine">
+      <Display />
+      <DrumPad />
+      {/* <App /> */}
+    </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
