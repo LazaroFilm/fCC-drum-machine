05f45b41781da281f75b131310e1d36472b6af4d
Victor Lazaro <lazarofilm@gmail.com>
2020-09-28 13:15:31 -0400

    Updates

---
+++ precache-manifest.a1ca695069937e7e617eccd757e8d738.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27da1d210aaf7bf7449c6578e8538150",
+    "url": "/fCC-drum-machine/index.html"
+  },
+  {
+    "revision": "655d80bcff763c316fb9",
+    "url": "/fCC-drum-machine/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "bdb16ba5a4bc9efce051",
+    "url": "/fCC-drum-machine/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "655d80bcff763c316fb9",
+    "url": "/fCC-drum-machine/static/js/2.4afca4d6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/fCC-drum-machine/static/js/2.4afca4d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdb16ba5a4bc9efce051",
+    "url": "/fCC-drum-machine/static/js/main.4c9d82f2.chunk.js"
+  },
+  {
+    "revision": "4045a56af40c468b867d",
+    "url": "/fCC-drum-machine/static/js/runtime-main.44af6009.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fCC-drum-machine/static/media/logo.5d5d9eef.svg"
+  }
+]);
