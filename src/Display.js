b3f632db9a608138e42eb65bf4bbcf722a62b488
Victor Lazaro <lazarofilm@gmail.com>
2020-09-28 18:22:19 -0400

    Added How to instructions

--- src/Display.js
+++ src/Display.js
@@ -1,5 +1,33 @@
 import React, { Component } from "react";
-import { Alert, Navbar } from "react-bootstrap";
+import { Alert, Navbar, Button } from "react-bootstrap";
+
+export function Instructions() {
+  const [show, setShow] = React.useState(true);
+
+  return (
+    <>
+      <Alert show={show} variant="success">
+        <Alert.Heading>How to use the dr(u/ea)m machine</Alert.Heading>
+        <p>
+          To start, click on any drum pad to activate the drum machine, then you
+          can press your keyboard keys!
+        </p>
+        <hr />
+        <div className="d-flex justify-content-end">
+          <Button onClick={() => setShow(false)} variant="outline-success">
+            Got it!
+          </Button>
+        </div>
+      </Alert>
+
+      {!show && (
+        <Button variant="light" onClick={() => setShow(true)}>
+          <sup>?</sup>
+        </Button>
+      )}
+    </>
+  );
+}
 
 class Display extends Component {
   render() {
